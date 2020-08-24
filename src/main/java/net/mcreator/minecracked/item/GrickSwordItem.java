725ae19ba0b8908b201462a4221d13feee1ee333
rainbowface2000 <Andrew@ANDREW-RM-DSKTOP>
2020-08-24 16:04:47 -0700

    Added Grick Sword

---
+++ src/main/java/net/mcreator/minecracked/item/GrickSwordItem.java
@@ -0,0 +1,64 @@
+
+package net.mcreator.minecracked.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.client.util.ITooltipFlag;
+
+import net.mcreator.minecracked.MinecrackedModElements;
+
+import java.util.List;
+
+@MinecrackedModElements.ModElement.Tag
+public class GrickSwordItem extends MinecrackedModElements.ModElement {
+	@ObjectHolder("minecracked:grick_sword")
+	public static final Item block = null;
+	public GrickSwordItem(MinecrackedModElements instance) {
+		super(instance, 8);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 100;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 8f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 3, -3.5f, new Item.Properties().group(ItemGroup.MISC)) {
+			@Override
+			public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+				super.addInformation(itemstack, world, list, flag);
+				list.add(new StringTextComponent("Stronger than diamond"));
+				list.add(new StringTextComponent("but at the cost of efficiency."));
+			}
+		}.setRegistryName("grick_sword"));
+	}
+}
--- src/main/java/net/mcreator/minecracked/item/GrickaxeItem.java
+++ src/main/java/net/mcreator/minecracked/item/GrickaxeItem.java
@@ -3,13 +3,22 @@ package net.mcreator.minecracked.item;
 
 import net.minecraftforge.registries.ObjectHolder;
 
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.IItemTier;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.block.BlockState;
 
+import net.mcreator.minecracked.procedures.GrickaxeBlockDestroyedWithToolProcedure;
 import net.mcreator.minecracked.MinecrackedModElements;
+
+import java.util.Map;
+import java.util.HashMap;
 
 @MinecrackedModElements.ModElement.Tag
 public class GrickaxeItem extends MinecrackedModElements.ModElement {
@@ -27,7 +36,7 @@ public class GrickaxeItem extends MinecrackedModElements.ModElement {
 			}
 
 			public float getEfficiency() {
-				return 10f;
+				return 4f;
 			}
 
 			public float getAttackDamage() {
@@ -35,7 +44,7 @@ public class GrickaxeItem extends MinecrackedModElements.ModElement {
 			}
 
 			public int getHarvestLevel() {
-				return 2;
+				return 3;
 			}
 
 			public int getEnchantability() {
@@ -46,6 +55,22 @@ public class GrickaxeItem extends MinecrackedModElements.ModElement {
 				return Ingredient.EMPTY;
 			}
 		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+			@Override
+			public boolean onBlockDestroyed(ItemStack itemstack, World world, BlockState bl, BlockPos pos, LivingEntity entity) {
+				boolean retval = super.onBlockDestroyed(itemstack, world, bl, pos, entity);
+				int x = pos.getX();
+				int y = pos.getY();
+				int z = pos.getZ();
+				{
+					Map<String, Object> $_dependencies = new HashMap<>();
+					$_dependencies.put("x", x);
+					$_dependencies.put("y", y);
+					$_dependencies.put("z", z);
+					$_dependencies.put("world", world);
+					GrickaxeBlockDestroyedWithToolProcedure.executeProcedure($_dependencies);
+				}
+				return retval;
+			}
 		}.setRegistryName("grickaxe"));
 	}
 }
