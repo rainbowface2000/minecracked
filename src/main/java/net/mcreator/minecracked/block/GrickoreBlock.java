0e87692ee8fccb75fe59a82fe7deb40d681bfd87
rainbowface2000 <Andrew@ANDREW-RM-DSKTOP>
2020-08-24 15:28:20 -0700

    blick and grick

--- src/main/java/net/mcreator/minecracked/block/GrickoreBlock.java
+++ src/main/java/net/mcreator/minecracked/block/GrickoreBlock.java
@@ -1,17 +1,46 @@
 
 package net.mcreator.minecracked.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.feature.OreFeature;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.minecracked.item.GrickItem;
+import net.mcreator.minecracked.MinecrackedModElements;
+
+import java.util.Random;
+import java.util.List;
+import java.util.Collections;
 
 @MinecrackedModElements.ModElement.Tag
 public class GrickoreBlock extends MinecrackedModElements.ModElement {
-
 	@ObjectHolder("minecracked:grickore")
 	public static final Block block = null;
-
 	public GrickoreBlock(MinecrackedModElements instance) {
 		super(instance, 4);
-
 	}
 
 	@Override
@@ -20,29 +49,21 @@ public class GrickoreBlock extends MinecrackedModElements.ModElement {
 		elements.items
 				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
 	}
-
 	public static class CustomBlock extends Block {
-
 		public CustomBlock() {
-			super(
-
-					Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).lightValue(0).harvestLevel(1)
-							.harvestTool(ToolType.PICKAXE));
-
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).lightValue(0).harvestLevel(1)
+					.harvestTool(ToolType.PICKAXE));
 			setRegistryName("grickore");
 		}
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(GrickItem.block, (int) (1)));
 		}
-
 	}
-
 	@Override
 	public void init(FMLCommonSetupEvent event) {
 		for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
@@ -51,19 +72,15 @@ public class GrickoreBlock extends MinecrackedModElements.ModElement {
 				biomeCriteria = true;
 			if (!biomeCriteria)
 				continue;
-
 			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, new OreFeature(OreFeatureConfig::deserialize) {
 				@Override
 				public boolean place(IWorld world, ChunkGenerator generator, Random rand, BlockPos pos, OreFeatureConfig config) {
 					DimensionType dimensionType = world.getDimension().getType();
 					boolean dimensionCriteria = false;
-
 					if (dimensionType == DimensionType.OVERWORLD)
 						dimensionCriteria = true;
-
 					if (!dimensionCriteria)
 						return false;
-
 					return super.place(world, generator, rand, pos, config);
 				}
 			}.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.create("grickore", "grickore", blockAt -> {
@@ -74,5 +91,4 @@ public class GrickoreBlock extends MinecrackedModElements.ModElement {
 			}), block.getDefaultState(), 8)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(10, 0, 0, 64))));
 		}
 	}
-
 }
