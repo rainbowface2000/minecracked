7d6591f4fde62805c7b4055201d4be98f2f9fa02
rainbowface2000 <Rainb@MSI>
2020-08-24 15:32:18 -0700

    Added Blick Axe Recipe

--- src/main/java/net/mcreator/minecracked/block/BlickOreBlock.java
+++ src/main/java/net/mcreator/minecracked/block/BlickOreBlock.java
@@ -1,17 +1,32 @@
 
 package net.mcreator.minecracked.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.minecracked.MinecrackedModElements;
+
+import java.util.List;
+import java.util.Collections;
 
 @MinecrackedModElements.ModElement.Tag
 public class BlickOreBlock extends MinecrackedModElements.ModElement {
-
 	@ObjectHolder("minecracked:blick_ore")
 	public static final Block block = null;
-
 	public BlickOreBlock(MinecrackedModElements instance) {
 		super(instance, 4);
-
 	}
 
 	@Override
@@ -20,15 +35,10 @@ public class BlickOreBlock extends MinecrackedModElements.ModElement {
 		elements.items
 				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
 	}
-
 	public static class CustomBlock extends Block {
-
 		public CustomBlock() {
-			super(
-
-					Block.Properties.create(Material.ROCK).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).lightValue(0).harvestLevel(1)
-							.harvestTool(ToolType.PICKAXE));
-
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).lightValue(0).harvestLevel(1)
+					.harvestTool(ToolType.PICKAXE));
 			setRegistryName("blick_ore");
 		}
 
@@ -40,13 +50,10 @@ public class BlickOreBlock extends MinecrackedModElements.ModElement {
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
-
 	}
-
 }
