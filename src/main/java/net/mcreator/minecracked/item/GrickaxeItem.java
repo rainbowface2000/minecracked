3c9f8cb6ccd03b78b8c620d05c7122804c7fce54
rainbowface2000 <Andrew@ANDREW-RM-DSKTOP>
2020-08-24 14:56:17 -0700

    Grickaxe added

---
+++ src/main/java/net/mcreator/minecracked/item/GrickaxeItem.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.minecracked.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.minecracked.MinecrackedModElements;
+
+@MinecrackedModElements.ModElement.Tag
+public class GrickaxeItem extends MinecrackedModElements.ModElement {
+	@ObjectHolder("minecracked:grickaxe")
+	public static final Item block = null;
+	public GrickaxeItem(MinecrackedModElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new PickaxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 100;
+			}
+
+			public float getEfficiency() {
+				return 10f;
+			}
+
+			public float getAttackDamage() {
+				return 2f;
+			}
+
+			public int getHarvestLevel() {
+				return 2;
+			}
+
+			public int getEnchantability() {
+				return 0;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("grickaxe"));
+	}
+}
