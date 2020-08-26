0618874809b9cb6519335da91745a4f4752dc41f
rainbowface2000 <Rainb@MSI>
2020-08-26 12:56:59 -0700

    Messing with Inventory Tick Changer

--- src/main/java/net/mcreator/minecracked/item/BlightArmorItem.java
+++ src/main/java/net/mcreator/minecracked/item/BlightArmorItem.java
@@ -1,21 +1,33 @@
 
 package net.mcreator.minecracked.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.minecracked.itemgroup.Minecraft2ItemGroup;
+import net.mcreator.minecracked.MinecrackedModElements;
+
 @MinecrackedModElements.ModElement.Tag
 public class BlightArmorItem extends MinecrackedModElements.ModElement {
-
 	@ObjectHolder("minecracked:blight_armor_helmet")
 	public static final Item helmet = null;
-
 	@ObjectHolder("minecracked:blight_armor_chestplate")
 	public static final Item body = null;
-
 	@ObjectHolder("minecracked:blight_armor_leggings")
 	public static final Item legs = null;
-
 	@ObjectHolder("minecracked:blight_armor_boots")
 	public static final Item boots = null;
-
 	public BlightArmorItem(MinecrackedModElements instance) {
 		super(instance, 18);
 	}
@@ -52,42 +64,29 @@ public class BlightArmorItem extends MinecrackedModElements.ModElement {
 				return 0f;
 			}
 		};
-
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.HEAD, new Item.Properties().group(Minecraft2ItemGroup.tab)) {
-
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
 				return "minecracked:textures/models/armor/blightedarmor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-
 		}.setRegistryName("blight_armor_helmet"));
-
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.CHEST, new Item.Properties().group(Minecraft2ItemGroup.tab)) {
-
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
 				return "minecracked:textures/models/armor/blightedarmor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-
 		}.setRegistryName("blight_armor_chestplate"));
-
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.LEGS, new Item.Properties().group(Minecraft2ItemGroup.tab)) {
-
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
 				return "minecracked:textures/models/armor/blightedarmor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-
 		}.setRegistryName("blight_armor_leggings"));
-
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.FEET, new Item.Properties().group(Minecraft2ItemGroup.tab)) {
-
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
 				return "minecracked:textures/models/armor/blightedarmor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-
 		}.setRegistryName("blight_armor_boots"));
 	}
-
 }
